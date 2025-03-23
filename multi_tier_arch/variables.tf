a44575f833d848838c8ce28de71c65ca92541a4a
jiyansh saini <rahulhbc@yahoo.in>
2025-03-24 00:10:10 +0530

    updated variable file

--- multi_tier_arch/variables.tf
+++ multi_tier_arch/variables.tf
@@ -1,9 +1,11 @@
 variable "resource_group_name" {
   description = "Name of the existing resource group"
   type        = string
+  
 }
 
 variable "location" {
   description = "Location of the resource group"
   type        = string
+  default     = "East US"
 }
