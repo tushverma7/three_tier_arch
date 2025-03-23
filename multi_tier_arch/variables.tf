1f6aaf0b708a6e4509fa7b561c43c0e227f96b6b
jiyansh saini <rahulhbc@yahoo.in>
2025-03-23 23:33:26 +0530

    Added the variable file

---
+++ multi_tier_arch/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  description = "Name of the existing resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Location of the resource group"
+  type        = string
+}
