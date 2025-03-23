02665dd1e8bc7af59a44c3668fbdd9344c75e68c
jiyansh saini <rahulhbc@yahoo.in>
2025-03-23 23:13:14 +0530

    Terraform format issue

--- multi_tier_arch/output.tf
+++ multi_tier_arch/output.tf
@@ -11,11 +11,11 @@ output "location" {
 # Frontend Network Interface (Add the correct resource name here)
 output "frontend_nic_id" {
   description = "The ID of the frontend network interface"
-  value       = azurerm_network_interface.frontend_nic.id  # Check for the correct resource name
+  value       = azurerm_network_interface.frontend_nic.id # Check for the correct resource name
 }
 
 # Frontend Public IP Address (Add the correct resource name here)
 output "frontend_public_ip" {
   description = "The public IP address of the frontend VM"
-  value       = azurerm_public_ip.frontend_pip.ip_address  # Check for the correct resource name
+  value       = azurerm_public_ip.frontend_pip.ip_address # Check for the correct resource name
 }
