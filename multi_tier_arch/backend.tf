34f9a874ba240f598125357b2cadb4f2d2c3fd8b
jiyansh saini <rahulhbc@yahoo.in>
2025-03-24 02:49:01 +0530

    updated backed.tf

--- multi_tier_arch/backend.tf
+++ multi_tier_arch/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "iac-secure-rg"
+    resource_group_name  = "RG-RBAC"
     storage_account_name = "storagelab001"
     container_name       = "3tier"
     key                  = "terraform.tfstate"
