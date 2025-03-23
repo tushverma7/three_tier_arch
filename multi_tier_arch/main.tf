9b4727ec9b77c435e19915b42f59849ab7606528
jiyansh saini <rahulhbc@yahoo.in>
2025-03-24 01:17:08 +0530

    updated main.tf

--- multi_tier_arch/main.tf
+++ multi_tier_arch/main.tf
@@ -1,39 +1,40 @@
-data "azurerm_resource_group" "rg" {
-  name = "iac-secure-rg"
+resource "azurerm_resource_group" "rg" {
+  name     = "iac-secure-rg"
+  location = "East US"
 }
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "iac-vnet"
-  resource_group_name = data.azurerm_resource_group.rg.name
-  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "frontend" {
   name                 = "frontend-subnet"
-  resource_group_name  = data.azurerm_resource_group.rg.name
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_subnet" "backend" {
   name                 = "backend-subnet"
-  resource_group_name  = data.azurerm_resource_group.rg.name
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_subnet" "database" {
   name                 = "database-subnet"
-  resource_group_name  = data.azurerm_resource_group.rg.name
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.3.0/24"]
 }
 
 resource "azurerm_network_security_group" "frontend_nsg" {
   name                = "frontend-nsg"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_network_security_rule" "allow_whitelisted_ips" {
@@ -46,7 +47,7 @@ resource "azurerm_network_security_rule" "allow_whitelisted_ips" {
   destination_port_range      = "80"
   source_address_prefixes     = ["192.168.0.0/24"] # Replace with your allowed IP
   destination_address_prefix  = "*"
-  resource_group_name         = data.azurerm_resource_group.rg.name
+  resource_group_name         = azurerm_resource_group.rg.name
   network_security_group_name = azurerm_network_security_group.frontend_nsg.name
 }
 
@@ -58,8 +59,8 @@ resource "azurerm_subnet_network_security_group_association" "frontend_assoc" {
 
 resource "azurerm_network_interface" "frontend_nic" {
   name                = "frontend-nic"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "frontend-ipconfig"
@@ -71,7 +72,7 @@ resource "azurerm_network_interface" "frontend_nic" {
 
 resource "azurerm_public_ip" "frontend_pip" {
   name                = "frontend-pip"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
 }
