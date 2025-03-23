50047a55671e1887169a74bf3f4ab9740a551439
jiyansh saini <rahulhbc@yahoo.in>
2025-03-23 14:17:40 +0530

    Initial commit to push thge terraform code on GitHub

---
+++ main.tf
@@ -0,0 +1,52 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "iac-secure-rg"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "iac-vnet"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "frontend" {
+  name                 = "frontend-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "backend" {
+  name                 = "backend-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_subnet" "database" {
+  name                 = "database-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
+
+resource "azurerm_network_security_group" "frontend_nsg" {
+  name                = "frontend-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_network_security_rule" "allow_whitelisted_ips" {
+  name                        = "Allow-Frontend-Access"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefixes     = ["192.168.0.101/24"] # Replace with your allowed IP
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.frontend_nsg.name
+}
---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = "7c63e1b6-0a9a-487e-b657-e58721d3e957"
+  tenant_id       = "f862763a-bd62-4c7e-bc68-befeaed1b9fb"
+}
