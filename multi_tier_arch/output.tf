9feeba47b587bb996b6789108e8f557bb27ee92b
jiyansh saini <rahulhbc@yahoo.in>
2025-03-23 23:11:55 +0530

    Changes in output.tf file

--- multi_tier_arch/output.tf
+++ multi_tier_arch/output.tf
@@ -1,14 +1,4 @@
-output "frontend_nic_id" {
-  description = "The ID of the frontend network interface"
-  value       = azurerm_network_interface.frontend_nic.id
-}
-
-output "frontend_public_ip" {
-  description = "The public IP address of the frontend VM"
-  value       = azurerm_public_ip.frontend_pip.ip_address
-}
-
-output "resource_group_name" {
+output "iac-secure-rg" {
   description = "The name of the resource group"
   value       = azurerm_resource_group.rg.name
 }
@@ -16,4 +6,16 @@ output "resource_group_name" {
 output "location" {
   description = "The location of the resource group"
   value       = azurerm_resource_group.rg.location
+}
+
+# Frontend Network Interface (Add the correct resource name here)
+output "frontend_nic_id" {
+  description = "The ID of the frontend network interface"
+  value       = azurerm_network_interface.frontend_nic.id  # Check for the correct resource name
+}
+
+# Frontend Public IP Address (Add the correct resource name here)
+output "frontend_public_ip" {
+  description = "The public IP address of the frontend VM"
+  value       = azurerm_public_ip.frontend_pip.ip_address  # Check for the correct resource name
 }
