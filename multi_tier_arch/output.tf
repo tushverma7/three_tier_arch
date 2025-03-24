a2c2e62b351efb1835fb062b65204003e41d4332
jiyansh saini <rahulhbc@yahoo.in>
2025-03-24 21:16:08 +0530

    updated config changes

--- multi_tier_arch/output.tf
+++ multi_tier_arch/output.tf
@@ -1,11 +1,11 @@
 output "resource_group_name" {
   description = "The name of the resource group"
-  value       = data.azurerm_resource_group.existing_rg.name
+  value       = azurerm_resource_group.existing_rg.name
 }
 
 output "location" {
   description = "The location of the resource group"
-  value       = data.azurerm_resource_group.existing_rg.location
+  value       = azurerm_resource_group.existing_rg.location
 }
 
 output "vnet_name" {
@@ -14,12 +14,18 @@ output "vnet_name" {
 }
 
 output "nsg_name" {
+  description = "Network Security Group name"
   value = azurerm_network_security_group.frontend_nsg.name
 }
 
 output "frontend_nic_id" {
   description = "The ID of the frontend network interface"
   value       = azurerm_network_interface.frontend_nic.id
+}
+
+output "public_ip" {
+  description = "Public IP address of the frontend VM"
+  value       = azurerm_public_ip.frontend_pip.ip_address
 }
 
 # Frontend Public IP Address (Add the correct resource name here)
