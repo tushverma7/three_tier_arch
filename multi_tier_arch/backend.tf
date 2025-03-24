11f91655f4c008574a2d649fd5c8f08ed687a575
jiyansh saini <rahulhbc@yahoo.in>
2025-03-24 20:34:41 +0530

    updated backend.tf pointing to correct AZURE RG

--- multi_tier_arch/backend.tf
+++ multi_tier_arch/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "RG-RBAC"
-    storage_account_name = "storagelab001"
+    resource_group_name  = "iac-secure-rg"
+    storage_account_name = "appdeploy001"
     container_name       = "3tier"
     key                  = "terraform.tfstate"
     subscription_id      = "7c63e1b6-0a9a-487e-b657-e58721d3e957"
