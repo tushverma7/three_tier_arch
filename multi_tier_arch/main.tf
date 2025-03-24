c964ab0c49e8c73a295572a4d9c0fece53788b82
jiyansh saini <rahulhbc@yahoo.in>
2025-03-25 01:50:47 +0530

    Added the security rule to NSG

--- multi_tier_arch/main.tf
+++ multi_tier_arch/main.tf
@@ -40,6 +40,18 @@ resource "azurerm_network_security_group" "frontend_nsg" {
   name                = "frontend-nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_network_security_rule" "allow_whitelisted_ips" {
